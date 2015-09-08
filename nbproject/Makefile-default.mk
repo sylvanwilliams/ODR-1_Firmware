87f50b5cb4ff9c162a334c80a292090fd34139bd
N7IFC <N7IFC@users.noreply.github.com>
2015-09-07 18:48:16 -0700

    add DSP files to project

--- nbproject/Makefile-default.mk
+++ nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c DSPIC33E_hardware.c LCD_driver.c LCD_fonts.c UI_main.c UI_page0.c UI_page1.c UI_page2.c si5351a.c TLV320AIC3204.c
+SOURCEFILES_QUOTED_IF_SPACED=main.c DSPIC33E_hardware.c LCD_driver.c LCD_fonts.c UI_main.c UI_page0.c UI_page1.c UI_page2.c si5351a.c TLV320AIC3204.c DSP.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/DSPIC33E_hardware.o ${OBJECTDIR}/LCD_driver.o ${OBJECTDIR}/LCD_fonts.o ${OBJECTDIR}/UI_main.o ${OBJECTDIR}/UI_page0.o ${OBJECTDIR}/UI_page1.o ${OBJECTDIR}/UI_page2.o ${OBJECTDIR}/si5351a.o ${OBJECTDIR}/TLV320AIC3204.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/DSPIC33E_hardware.o.d ${OBJECTDIR}/LCD_driver.o.d ${OBJECTDIR}/LCD_fonts.o.d ${OBJECTDIR}/UI_main.o.d ${OBJECTDIR}/UI_page0.o.d ${OBJECTDIR}/UI_page1.o.d ${OBJECTDIR}/UI_page2.o.d ${OBJECTDIR}/si5351a.o.d ${OBJECTDIR}/TLV320AIC3204.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.o ${OBJECTDIR}/DSPIC33E_hardware.o ${OBJECTDIR}/LCD_driver.o ${OBJECTDIR}/LCD_fonts.o ${OBJECTDIR}/UI_main.o ${OBJECTDIR}/UI_page0.o ${OBJECTDIR}/UI_page1.o ${OBJECTDIR}/UI_page2.o ${OBJECTDIR}/si5351a.o ${OBJECTDIR}/TLV320AIC3204.o ${OBJECTDIR}/DSP.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/main.o.d ${OBJECTDIR}/DSPIC33E_hardware.o.d ${OBJECTDIR}/LCD_driver.o.d ${OBJECTDIR}/LCD_fonts.o.d ${OBJECTDIR}/UI_main.o.d ${OBJECTDIR}/UI_page0.o.d ${OBJECTDIR}/UI_page1.o.d ${OBJECTDIR}/UI_page2.o.d ${OBJECTDIR}/si5351a.o.d ${OBJECTDIR}/TLV320AIC3204.o.d ${OBJECTDIR}/DSP.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/DSPIC33E_hardware.o ${OBJECTDIR}/LCD_driver.o ${OBJECTDIR}/LCD_fonts.o ${OBJECTDIR}/UI_main.o ${OBJECTDIR}/UI_page0.o ${OBJECTDIR}/UI_page1.o ${OBJECTDIR}/UI_page2.o ${OBJECTDIR}/si5351a.o ${OBJECTDIR}/TLV320AIC3204.o
+OBJECTFILES=${OBJECTDIR}/main.o ${OBJECTDIR}/DSPIC33E_hardware.o ${OBJECTDIR}/LCD_driver.o ${OBJECTDIR}/LCD_fonts.o ${OBJECTDIR}/UI_main.o ${OBJECTDIR}/UI_page0.o ${OBJECTDIR}/UI_page1.o ${OBJECTDIR}/UI_page2.o ${OBJECTDIR}/si5351a.o ${OBJECTDIR}/TLV320AIC3204.o ${OBJECTDIR}/DSP.o
 
 # Source Files
-SOURCEFILES=main.c DSPIC33E_hardware.c LCD_driver.c LCD_fonts.c UI_main.c UI_page0.c UI_page1.c UI_page2.c si5351a.c TLV320AIC3204.c
+SOURCEFILES=main.c DSPIC33E_hardware.c LCD_driver.c LCD_fonts.c UI_main.c UI_page0.c UI_page1.c UI_page2.c si5351a.c TLV320AIC3204.c DSP.c
 
 
 CFLAGS=
@@ -149,6 +149,13 @@ ${OBJECTDIR}/TLV320AIC3204.o: TLV320AIC3204.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} $(MP_EXTRA_CC_PRE)  TLV320AIC3204.c  -o ${OBJECTDIR}/TLV320AIC3204.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/TLV320AIC3204.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -omf=elf -mlarge-data -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/TLV320AIC3204.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
+${OBJECTDIR}/DSP.o: DSP.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/DSP.o.d 
+	@${RM} ${OBJECTDIR}/DSP.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  DSP.c  -o ${OBJECTDIR}/DSP.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/DSP.o.d"      -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -omf=elf -mlarge-data -O0 -msmart-io=1 -Wall -msfr-warn=off
+	@${FIXDEPS} "${OBJECTDIR}/DSP.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
 else
 ${OBJECTDIR}/main.o: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
@@ -219,6 +226,13 @@ ${OBJECTDIR}/TLV320AIC3204.o: TLV320AIC3204.c  nbproject/Makefile-${CND_CONF}.mk
 	@${RM} ${OBJECTDIR}/TLV320AIC3204.o 
 	${MP_CC} $(MP_EXTRA_CC_PRE)  TLV320AIC3204.c  -o ${OBJECTDIR}/TLV320AIC3204.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/TLV320AIC3204.o.d"      -g -omf=elf -mlarge-data -O0 -msmart-io=1 -Wall -msfr-warn=off
 	@${FIXDEPS} "${OBJECTDIR}/TLV320AIC3204.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
+	
+${OBJECTDIR}/DSP.o: DSP.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/DSP.o.d 
+	@${RM} ${OBJECTDIR}/DSP.o 
+	${MP_CC} $(MP_EXTRA_CC_PRE)  DSP.c  -o ${OBJECTDIR}/DSP.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/DSP.o.d"      -g -omf=elf -mlarge-data -O0 -msmart-io=1 -Wall -msfr-warn=off
+	@${FIXDEPS} "${OBJECTDIR}/DSP.o.d" $(SILENT)  -rsi ${MP_CC_DIR}../ 
 	
 endif
 
